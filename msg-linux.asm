27bfa140a1f6078a17f6113a3afdb8b951a260c3
Nosferatuvjr <vivaldochagassjr@gmail.com>
2022-04-09 19:52:17 -0400

    assembly is love

--- msg-linux.asm
+++ msg-linux.asm
@@ -1,11 +1,14 @@
 ;Nesse arquivo aprendi um pouco sobre como funcionam as interrupções e como usá-las.
 ;Esse arquivo é um exemplo apenas para linux
+;Para compilar: #apt install update && apt install upgrade && apt install nasm
+;#nasm -f elf32 demo.asm && ld -m elf_i386 demo.o -o demo
+;Para executar: #./demo
 
 section .data
-msg db "Isso é uma mensagem de teste",0xA
-len equ $-msg
+msg db "Isso é uma mensagem de teste",0xA           ;Pula linha com 0xA
+len equ $-msg                                       ;Calcula a posição atual "-" (menos) a posição de msg
 
-msg2 db "Isso é uma mensagem de teste 2",0xA
+msg2 db "Isso é uma mensagem de teste 2",0xA        ;Pula linha com 0xA
 len2 equ $-msg2
 
 section .text
@@ -13,26 +16,26 @@ global _start
 
 _start:
 
-mv edx, len                                        ;Pega o tamanho da mensagem e armazena em edx
-mv ecx, msg                                        ;
-mv ebx, 1                                          ;
-mv eax, 4                                          ;
-int 0x80                                           ;
+mov edx, len                                        ;Pega o tamanho da mensagem e armazena em edx
+mov ecx, msg                                        ;Endereço da mensagem
+mov ebx, 1                                          ;Saída padrão da chamada
+mov eax, 4                                          ;
+int 0x80                                           ;Primeira interrupição
 
 call _anotherfunction
 
-mv ebx, 0
-mv eax, 1
+mov ebx, 0
+mov eax, 1
 int 0x80                                           ;Segunda interrupção
 
 _anotherfunction:
 push ebp
-mv ebp, esp
+mov ebp, esp
 
-mv edx, len2                                       ;Pega o tamanho da mensagem2 e armazena em edx
-mv ecx, msg2
-mv evx, 1
-mv eax, 4
+mov edx, len2                                       ;Pega o tamanho da mensagem2 e armazena em edx
+mov ecx, msg2
+mov ebx, 1
+mov eax, 4
 int 0x80                                           ;Terceira interrupção
 
 pop ebp
